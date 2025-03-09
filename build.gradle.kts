30e3aff16b3955283190079eb3b30996dd2ab440
lpartsas <lpartsas@skroutz.gr>
2025-03-09 13:51:48 +0200

    Publish to github packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import java.net.URI
+
 plugins {
     kotlin("jvm") version "2.1.10"
     kotlin("plugin.serialization") version "1.9.0"
+    id("maven-publish")
 }
 
 group = "gr.leonidas"
@@ -14,6 +17,19 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     testImplementation(kotlin("test"))
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = URI.create("https://maven.pkg.github.com/le0nidas/ollama-kotlin-playground")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
 
 tasks.test {
